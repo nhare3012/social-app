12e8c3163b56f4dc589b9c6f8b3d38f0b5b991b5
nhare3012 <nharetawanda@gmail.com>
2022-01-13 15:13:52 +0200

    home wrapper

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,11 +1,33 @@
 import React from 'react'
-import Topbar from '../../components/topbar/Topbar'
+import Topbar from '../../components/topbar/Topbar';
+import Sidebar from '../../components/sidebar/Sidebar'
 
 function Home() {
     return (
         <div>
-            <Topbar/>
-            
+          
+          <Topbar/>
+          <div className="homeContainer">
+             <div className="row">
+                 <div className="col-md-3">
+                     <div className="side">
+                         <Sidebar/> 
+                     </div>
+                 </div>
+                 <div className="col-md-5">
+                     <div className="feed">
+
+                     </div>
+                 </div>
+                 <div className="col-md-4">
+                     <div className="rightBar">
+                         
+                     </div>
+                 </div>
+             </div>
+
+          </div>
+             
         </div>
     )
 }
